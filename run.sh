53769fd7dc71ed855d9290bccb888263c9b60d46
Fluturenet <fluturenet@gmail.com>
2017-12-26 23:27:56 +0100

    Update run.sh
    starting version 3.2.4
    jq: error: UsbPath/0 is not defined at <top-level>, line 1:
    UsbPath
    jq: 1 compile error
    
    starting version 3.2.4
    /run.sh: line 8: /bin/socat: No such file or directory

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ set -e
 
 CONFIG_PATH=/data/options.json
 
-USB=$(jq --raw-output "UsbPath" $CONFIG_PATH)
+USB=$(jq --raw-output ".UsbPath" $CONFIG_PATH)
 
-/bin/socat TCP-LISTEN:20000,fork $USB,raw,b115200,echo=0
+/usr/bin/socat TCP-LISTEN:20000,fork $USB,raw,b115200,echo=0
